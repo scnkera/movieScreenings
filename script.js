4cdab92cc6fd01336ac089f2b0c5b8a414d0e337
scnkera <149810869+scnkera@users.noreply.github.com>
2024-06-04 23:26:25 -0400

    Updated HTML, CSS, and JavaScript

--- script.js
+++ script.js
@@ -29,7 +29,11 @@ const movieDropdown = document.getElementById("movieSelection");
 // console.log(movieDropdown);
 // #5 Log out the key value pairs using Object.entries function (built into JS) so we
 //can see the structure of the data before we loop over it.
-console.log(Object.entries(movies));
+// console.log(Object.entries(movies));
+
+// for (const data of Object.values(movies)) {
+// 	console.log(data);
+// }
 
 // #6 Create new variable called options that starts as a string with an empty option element
 let options = "<option></option>";
@@ -40,7 +44,7 @@ let options = "<option></option>";
 for (const [movieTitle, movieData] of Object.entries(movies)) {
 	//#8 Log out both the movieTitle and the price to make sure our loop is running over each
 	//movie and we can confirm we have access to the data we care about
-	console.log(movieTitle, movieData.price);
+	// console.log(movieTitle, movieData.price);
 
 	//The line below is a way to avoid template literals and use string concatentation instead
 	// options +=
@@ -63,23 +67,89 @@ for (const [movieTitle, movieData] of Object.entries(movies)) {
 // #10 Using all the options we generated from #9, set that into the <select> tag from #3
 movieDropdown.innerHTML = options;
 
+// #17 User change the movie in the dropdown
 movieDropdown.onchange = (event) => {
+	//#18 Retrieve what movie they selected as a string
 	const selectedMovie = event.target.value;
-	console.log(selectedMovie);
-	console.log(movies[selectedMovie]);
+	// #19 Use string from #18 to get the corresponding movieData from the obj from #2
+	const movieData = movies[selectedMovie];
+	console.log(movieData.seats);
+
+	// #20 Start an empty string that we can eventually add HTML to
+	let seatsHTML = "";
+
+	// #21 Using Object.entries, get access to the rows of seats from #19 and also the index so
+	//we can keep track of what row num we are on
+	for (let [rowIndex, row] of Object.entries(movieData.seats)) {
+		// #22 Add the opening div tag for our row to the string
+		seatsHTML += "<div>";
+		// #23 Loop over all the cols in the row from #21
+		for (let [colIndex, col] of Object.entries(row)) {
+			console.log(`Row: ${rowIndex} & Col: ${colIndex}`);
+
+			// #24 Generate a shitload of HTML using a template literal. This not only generates
+			//the span tag with the chair as the contents, it also looks at the col we are looping over
+			//to see if it is occupied using ternary operator. If it is, we add the class named "occpied".
+			//We also use the row & col index that our favorite Object.entries method gives us access to from
+			//#21 & 23 to include as HTML attributes so when the chair is clicked on, we can use that data to
+			//know each one. Finally we add an onclick event, so when the chair is clicked on, we can fire an
+			//event listener.
+			seatsHTML += `<span 
+			class="material-symbols-outlined ${col.occupied ? "occupied" : ""}" 
+			data-rowIndex="${rowIndex}"
+			data-colIndex="${colIndex}"
+			onclick="seatClicked(event)"
+			> chair </span>`;
+		}
+		// #25 After we are done generating these 8 seats, add the closing div tag to match #22
+		seatsHTML += "</div>";
+	}
+
+	//#26 We are done generating all the seats, so add in the screen
+	seatsHTML += `<div id="screen">Screen</div>`;
+
+	//#27 Take all the HTML that we have generated as a string and set it into the seats div.
+	//This is the step that JS stops treating our HTML as a string, and actually makes the browser show it.
+	document.getElementById("seats").innerHTML = seatsHTML;
 };
 
+// #11 Loop over all of the values from the movies obj from #2
 for (const movieData of Object.values(movies)) {
 	//start of for loop for each movie
-	console.log(movieData);
+	// console.log(movieData);
+	// #12 For each movie, start a loop, that will run 8 times
 	for (let rowIterator = 0; rowIterator < 8; rowIterator++) {
 		//start of for loop for each row
+		// #13 Create an empty array so we have a place to store each col of seats
 		let row = [];
+		// #14 Write a for loop that also runs 8 times for each of the columns
 		for (let colIterator = 0; colIterator < 8; colIterator++) {
 			//start of for loop for each col
-			row.push({});
+			// #15 Push an empty obj (which will eventually include the seat data) for all
+			//8 cols AND all eight rows AND all four movies
+			row.push({ occupied: Math.random() < 0.5 });
 		} // end of for loop for each col
+		//#16 After generating the entire 8 cols of seats, push the row we generated into the
+		//seats key of the movieData object which we have one of for each movie
 		movieData.seats.push(row);
 	} //end of for loop for each row
 } //end of for loop for each movie
+
 console.log(movies);
+
+// #28 Wait until a seat is clicked on
+const seatClicked = (event) => {
+	console.log("Seat clicked");
+	// #29 Retrieve the value from the select so we know what movie is active
+	const selectedMovie = document.getElementById("movieSelection").value;
+	console.log(selectedMovie);
+	// #30 Retrieve the rowIndex & colIndex from #24 based off what the event target was
+	// Ie what was clicked on
+	const rowIndex = event.target.getAttribute("data-rowIndex");
+	const colIndex = event.target.getAttribute("data-colIndex");
+
+	// #31 Use the data from #29 & #30 to access our main object from #2 to get out the
+	//specific seat we care about.
+	const seat = movies[selectedMovie].seats[rowIndex][colIndex];
+	console.log(seat);
+};
